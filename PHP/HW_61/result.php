d51d47416b0b98e38e8a479d6d33e9b7421d4158
Shmuel Sochet <sysochet@gmail.com>
2017-07-10 21:04:07 -0400

    Fix header

--- PHP/HW_61/result.php
+++ PHP/HW_61/result.php
@@ -1,5 +1,5 @@
 <?php
-    $month = 0;
+    $month = "";
     $millenium = 0;
     $century = 0;
     $decade = 0;
@@ -27,7 +27,7 @@
         if(! in_array($_POST['month'], $months)) {
             
             $month = $_POST['month'];
-            $errors[] = "$month is not a valid month";
+            $errors[] = "$month is not a valid month.";
              
             
         }else{
@@ -35,59 +35,65 @@
         }
         
     }else {
-        $errors[] = "Month is a required field";
+        $errors[] = "Month is a required field.";
         
     }
 
     if( isset($_POST['millenium'])) {
-        if(! is_numeric($_POST['millenium']) || $_POST['millenium'] < 0 || $_POST['millenium'] > 9) {
+        if(! is_numeric($_POST['millenium']) || $_POST['millenium'] < 1 || $_POST['millenium'] > 9) {
   
-            $errors[] = "Millenium must be a number greater than -1 and less than 9";
+            $errors[] = "Millenium must be a number greater than 0 and less than 9 (Gregorian calendar began in year 1592).";
             
         }
-        $year = $_POST['millenium'];
-    } else {
-        $errors[] ="Millenium is a required field";
+        $millenium = $_POST['millenium'];
+    } else  {
+        $errors[] ="Millenium is a required field.";
     }
 
     if( isset($_POST['century'])) {
         if(! is_numeric($_POST['century']) || $_POST['century'] < 0 || $_POST['century'] > 9) {
   
-            $errors[] = "Century must be a number greater than -1 and less than 9";
+            $errors[] = "Century must be a number greater than -1 and less than 9.";
             
+        }elseif($_POST['millenium'] < 2 && $_POST['century'] < 5 ){
+            $errors[] = "Century must be a number greater than 5, if Millenium is less than 2 (Gregorian calendar began in year 1592).";
         }
-        $year = $_POST['century'];
+        $century = $_POST['century'];
     } else {
-        $errors[] ="Century is a required field";
+        $errors[] ="Century is a required field.";
     }
 
     if( isset($_POST['decade'])) {
         if(! is_numeric($_POST['decade']) || $_POST['decade'] < 0 || $_POST['decade'] > 9) {
   
-            $errors[] = "Decade must be a number greater than -1 and less than 9";
+            $errors[] = "Decade must be a number greater than -1 and less than 9.";
             
+        }elseif($_POST['millenium'] < 2 && $_POST['century'] < 6  &&  $_POST['decade'] < 8){
+             $errors[] = "Decade must be a number greater than 8, if Millenium is less than 2 and Century is less than 6 (Gregorian calendar began in year 1592).";
         }
-        $year = $_POST['decade'];
+        $decade = $_POST['decade'];
     } else {
-        $errors[] ="Decade is a required field";
+        $errors[] ="Decade is a required field.";
     }
 
     if( isset($_POST['year'])) {
         if(! is_numeric($_POST['year']) || $_POST['year'] < 0 || $_POST['year'] > 9) {
   
-            $errors[] = "Year must be a number greater than -1 and less than 9";
+            $errors[] = "Year must be a number greater than -1 and less than 9.";
             
+        }elseif($_POST['millenium'] < 2 && $_POST['century'] < 6  &&  $_POST['decade'] < 8 && $_POST['year'] < 2){
+             $errors[] = "Year must be a number greater than 1, if Millenium is less than 2 and Century is less than 6 and Decade is 9 (Gregorian calendar began in year 1592).";
         }
         $year = $_POST['year'];
     } else {
-        $errors[] ="Year is a required field";
+        $errors[] ="Year is a required field.";
     }
         
-    $month = $_POST['month'];
-    $millenium = $_POST['millenium'];
-    $century = $_POST['century'];
-    $decade = $_POST['decade'];
-    $year = $_POST['year'];
+    // $month = $_POST['month'];
+    // $millenium = $_POST['millenium'];
+    // $century = $_POST['century'];
+    // $decade = $_POST['decade'];
+    // $year = $_POST['year'];
 
     function isLeapYear($millenium, $century, $decade, $year){
         $fullYear = $millenium . $century . $decade . $year;
@@ -105,13 +111,13 @@
         return $answer;
     }
 
-    function getMonthDays($month){
-        
-        $month = $_POST['month'];
-        $millenium = $_POST['millenium'];
-        $century = $_POST['century'];
-        $decade = $_POST['decade'];
-        $year = $_POST['year'];
+    function getMonthDays($month, $millenium, $century, $decade, $year){
+
+        // $month = $_POST['month'];
+        // $millenium = $_POST['millenium'];
+        // $century = $_POST['century'];
+        // $decade = $_POST['decade'];
+        // $year = $_POST['year'];
         
         $monthDays = [
 
@@ -165,34 +171,37 @@
 <body>
     <div class="container">
         <div class="jumbotron text-center">
-            <h1>PCS Interest Calculator</h1>
+            <h1>Is it a Leap Year?</h1>
             <h2>Results</h2>
         </div>
 
         <div class="container">
-        <?php if (!empty($errors)) : ?>
-        <div class="alert alert-danger">
-            <ul>
-                <?php foreach($errors as $error) echo "<li>$error</li>" ?>
-            </ul>
-            <?php endif ?>
+            <?php if (!empty($errors)) : ?>
+            <div class="alert alert-danger">
+                <ul>
+                    <?php foreach($errors as $error) echo "<li>$error</li>" ?>
+                </ul>
+                <?php endif ?>
+            </div>
         </div>
 
-        <div>
-            <div>
-                <div class="well col-sm-2 text-right">Month</div>
+        <div class= "container">
+            <div class="row">
+                <div class="well col-sm-2 col-sm-offset-2 text-right">Month</div>
                 <div class="col-sm-4 well text-center"><?= $month ?></div>
             </div>
-            <div>
-                <div class="well col-sm-1 text-right">Year</div>
+            <div class="row">
+                <!--<div class="well col-sm-1 col-sm-offset-3 text-right">Year</div>
                 <div class="col-sm-1 well text-center"><?= $millenium ?></div>
                 <div class="col-sm-1 well text-center"><?= $century ?></div>
                 <div class="col-sm-1 well text-center"><?= $decade ?></div>
-                <div class="col-sm-1 well text-center"><?= $year ?></div>
+                <div class="col-sm-1 well text-center"><?= $year ?></div>-->
+                <div class="well col-sm-2 col-sm-offset-2 text-right">Year</div>
+                <div class="col-sm-4 well text-center"><?= $millenium . $century . $decade . $year; ?></div>
             </div>
-            <div>
-                <div class="well col-sm-2 text-right">How Many Days</div>
-                <div class="col-sm-4 well"><?= getMonthDays($month)  ?></div>
+            <div class="row">
+                <div class="well col-sm-2 col-sm-offset-2 text-right">How Many Days</div>
+                <div class="col-sm-4 well text-center"><?= getMonthDays($month, $millenium, $century, $decade, $year)  ?></div>
             </div>
         </div>
 
