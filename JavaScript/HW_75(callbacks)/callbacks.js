b8515408276df4d204e464f61c22a91d1cc8d791
ssochet <sysochet@gmail.com>
2017-09-19 22:59:44 -0400

    Add HW_75 (callbacks)

---
+++ JavaScript/HW_75(callbacks)/callbacks.js
@@ -0,0 +1,60 @@
+function isUpperCase(element, index, array) {
+    return element === element.toUpperCase();
+}
+
+function isLowerCase(element, index, array) {
+    return element === element.toLowerCase();
+}
+
+function some(array, callback) {
+    for (i = 0; i < array.length; i++) {
+        if (callback(array[i]) === true) {
+            console.log(true);
+            return true;
+        }
+    }
+    console.log(false);
+    return false;
+
+}
+
+var arr = ["a", "b", "c"];
+
+some(arr, isUpperCase);
+
+some(arr, isLowerCase);
+
+console.log(arr.some(isUpperCase));
+
+console.log(arr.some(isLowerCase));
+
+function test(element) {
+    return element > 0;
+}
+
+function action(element) {
+    return element += 5;
+}
+
+function onlyIf(array, test, action) {
+    var arrayToTest = [];
+    var arrayWithAction = [];
+    array.forEach(function (element) {
+        if (test(element)) {
+            arrayToTest.push(element);
+        }
+    });
+    arrayToTest.forEach(function (element) {
+        element = action(element);
+        arrayWithAction.push(element);
+    });
+
+    return arrayWithAction;
+}
+
+var arr2 = [0, 2, 4, 6, -1];
+var a = onlyIf(arr2, test, action);
+console.log(a);
+
+
+
