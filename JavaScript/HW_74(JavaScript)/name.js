0dc50d106d69c4b9f60050816420f3215ba471e5
ssochet <sysochet@gmail.com>
2017-09-24 16:05:27 -0400

    add test code example

--- JavaScript/HW_74(JavaScript)/name.js
+++ JavaScript/HW_74(JavaScript)/name.js
@@ -11,3 +11,7 @@ function greaterThan(first, second) {
 console.log("7 is greater than 6:" + greaterThan(7, 6));
 console.log("3 is greater than 6:" + greaterThan(3, 6));
 console.log("6 is greater than 6:" + greaterThan(6, 6));
+
+//this would be the test code for the function greaterThan and some write it before writing the actual code
+console.assert(greaterThan(5, 6) === false);
+console.assert(greaterThan(6, 5) === true);
