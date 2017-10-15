e6513397bd3f057b2c6c98e10b5491084384c00b
ssochet <sysochet@gmail.com>
2017-10-14 22:29:22 -0400

    changes based on S. Lubowsky's class

--- JavaScript/HW_75(callbacks)/callbacks.js
+++ JavaScript/HW_75(callbacks)/callbacks.js
@@ -1,28 +1,34 @@
-function isUpperCase(element, index, array) {
-    return element === element.toUpperCase();
+"use strict";
+function isUpperCase(element) {
+
+    //this will check if it's letter and if uppercase
+    return typeof element === "string" &&
+        element === element.toUpperCase();
 }
 
-function isLowerCase(element, index, array) {
+function isLowerCase(element) {
+    //better to write below since if you need to change one you don't have to change the 2nd
+    //though it's more expensive to have a function call then a regular return
+    // return !isUpperCase(); 
     return element === element.toLowerCase();
 }
 
 function some(array, callback) {
-    for (i = 0; i < array.length; i++) {
+    for (var i = 0; i < array.length; i++) {
         if (callback(array[i]) === true) {
-            console.log(true);
             return true;
         }
     }
-    console.log(false);
     return false;
 
 }
 
 var arr = ["a", "b", "c"];
 
-some(arr, isUpperCase);
+//these are test code that some may write first before writing the function
+console.log(some(arr, isUpperCase));
 
-some(arr, isLowerCase);
+console.log(some(arr, isLowerCase));
 
 console.log(arr.some(isUpperCase));
 
@@ -36,19 +42,32 @@ function action(element) {
     return element += 5;
 }
 
+//this was to create a new array with the elements that passed the test and had the action
+//performed on them
+// function onlyIf(array, test, action) {
+//     var arrayToTest = [];
+//     var arrayWithAction = [];
+//     array.forEach(function (element) {
+//         if (test(element)) {
+//             arrayToTest.push(element);
+//         }
+//     });
+//     arrayToTest.forEach(function (element) {
+//         element = action(element);
+//         arrayWithAction.push(element);
+//     });
+//     return arrayWithAction;
+// }
+
+//or for functional programming you would create a new array and leave the original untouched
 function onlyIf(array, test, action) {
-    var arrayToTest = [];
-    var arrayWithAction = [];
-    array.forEach(function (element) {
+    array.forEach(function (element, index) {
         if (test(element)) {
-            arrayToTest.push(element);
+            array[index] = action(element);
         }
     });
-    arrayToTest.forEach(function (element) {
-        element = action(element);
-        arrayWithAction.push(element);
-    });
-    return arrayWithAction;
+    return array;
+
 }
 
 var originalArr = [0, 2, 4, 6, -1];
