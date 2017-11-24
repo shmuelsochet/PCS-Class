d61259f9f7bcf19bf3687dcfb66df32aa71265ff
ssochet <sysochet@gmail.com>
2017-11-23 20:02:24 -0500

    changes based on class

--- JavaScript/HW_85_ajax/ajax.js
+++ JavaScript/HW_85_ajax/ajax.js
@@ -1,19 +1,35 @@
 /* global $ */
-/* global jQuery */
 (function () {
     "use strict";
 
-    jQuery('#load').click('click', function () {
+    //wrap first so you don't have to wrap every time you click the click handler
+    var theFileInputName = $('#file');
+
+    $('#load').click('click', function () {
 
         $('#loading').show();
 
-        $.get(jQuery('#file').val(), function (loadedData) {
-            $("body").append(loadedData);
-            $('#loading').hide();
+        $.get(theFileInputName.val(), function (loadedData) {
+            setTimeout(function () {
+                $("#loadedText").append(loadedData);
+
+            }, 2000);
+
         })
-            .fail(function (xhr, statusCode, statusText) {
-                $('.result').append('Error! ' + 'Status Code: ' + statusCode + '. Status Text ' + statusText + '.');
+            .fail(function (jqxhr, statusCode, statusText) {
+                $('#loadedText').append('Error! ' + 'Status Code: ' + statusCode + '. Status Text ' + statusText + '. ');
+                console.log(jqxhr);
+            })
+            //this will always compute whether fail or success
+            .always(function () {
+                setTimeout(function () {
+
+                    $('#loading').hide();
+                }, 2000);
+
             });
+
+
     });
 
 
