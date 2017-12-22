ec0f7b9b51c05f90a54c24f95c3bda8224fb255b
ssochet <sysochet@gmail.com>
2017-12-21 20:15:53 -0500

    A few small changes

--- JavaScript/HW_88_video/video.php
+++ JavaScript/HW_88_video/video.php
@@ -1,8 +1,11 @@
 <?php
+
+//this isn't hooked up yet
 require 'db.php';
-$query = "SELECT * FROM recipes";
+$query = "SELECT * FROM videos";
 $stmt = $db->query($query);
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($rows);
+
 ?>
