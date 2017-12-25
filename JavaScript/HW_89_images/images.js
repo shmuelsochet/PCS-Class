39d14122939b99890cb42837711c440739f925f0
ssochet <sysochet@gmail.com>
2017-12-24 21:19:32 -0500

    formatting fixes

--- JavaScript/HW_89_images/images.js
+++ JavaScript/HW_89_images/images.js
@@ -8,19 +8,17 @@
         var imageTitle = $("h1");
         var rightBtn = $("#right");
         var leftBtn = $("#left");
-        //var currentImage;
         var currentIndex = 0;
 
         imageList.forEach(function (image, index) {
             $('<li><img src="' + (image.url || 'videos/default.png') + '"/>' + image.title + '</li>')
                 .appendTo(theList)
                 .click(function () {
-                    //currentImage = image.url;
                     currentIndex = index;
                     imageTitle.text(image.title);
                     imageViewer.attr("src", image.url);
                     imageViewer.show();
-                    //videoPlayer[0].play();
+
                 });
         });
 
