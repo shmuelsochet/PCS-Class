4558867946a7f7e93b7435cc8ce286118bcc96fd
ssochet <sysochet@gmail.com>
2017-12-31 12:24:24 -0500

    Add more features, wiki link, info window.

--- JavaScript/HW_91_maps_wiki/maps.js
+++ JavaScript/HW_91_maps_wiki/maps.js
@@ -3,15 +3,15 @@
 
     "use strict";
 
-
-    var uluru = { lat: 40.741895, lng: -73.989308 };
+    var coordinates = { lat: 40.106440, lng: -74.232701 };
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
+        zoom: 25,
+        center: coordinates,
+        mapTypeId: 'satellite'
     });
     var marker = new google.maps.Marker({
-        position: uluru,
+        position: coordinates,
         map: map
     });
 
@@ -19,7 +19,10 @@
         submitButton = $('#submitButton'),
         theList = $('#sidebar ul');
 
-    var isEmpty = true;
+    var isEmpty = true,
+        labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        labelIndex = 0;
+
 
     submitButton.click(function () {
         $.getJSON("http://api.geonames.org/wikipediaSearch?q=" + searchBox.val() + "&maxRows=100&username=shmuels&type=json&callback=?",
@@ -33,10 +36,41 @@
                     $('<li><img src="' + (element.thumbnailImg) + '"/>' + element.title + '</li>')
                         .appendTo(theList)
                         .click(function () {
+
+                            coordinates = { lat: element.lat, lng: element.lng };
+
                             var marker = new google.maps.Marker({
-                                position: { lat: element.lat, lng: element.lng },
-                                map: map
+                                position: coordinates,
+                                map: map,
+                                title: element.title,
+                                label: labels[labelIndex++ % labels.length],
                             });
+
+                            var contentString = '<div >' +
+                                '<div>' +
+                                '</div>' +
+                                '<h1>' + element.title + '</h1>' +
+                                '<div>' +
+                                '<p><b>' + element.title + '.</b> ' + element.summary + '</p>' +
+                                '<p>Attribution: ' + element.title + ', <a target=_blank' +
+                                'href=https://' + element.wikipediaUrl + '>' + element.wikipediaUrl + '</a> ' +
+                                '.</p>' +
+                                '</div>' +
+                                '</div>';
+
+                            var infoWindow = new google.maps.InfoWindow({
+                                content: contentString
+
+                            });
+
+                            map.setCenter(coordinates);
+
+                            marker.addListener('click', function () {
+                                infoWindow.open(map, marker);
+                            });
+
+
+
                         });
 
                 });
