bb83b6dc288039bfc1846f38b52a11ccb03d9f46
ssochet <sysochet@gmail.com>
2018-01-03 20:06:22 -0500

    Remove HW 92 drawing code from HW 91 files.

--- JavaScript/HW_91_maps_wiki/maps.js
+++ JavaScript/HW_91_maps_wiki/maps.js
@@ -17,42 +17,13 @@ function initMap() {
         map: map
     });
 
-    new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.MARKER,
-        drawingControl: true,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_CENTER,
-            drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-        },
-        markerOptions: { icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' },
-        circleOptions: {
-            fillColor: '#ffff00',
-            fillOpacity: 1,
-            strokeWeight: 5,
-            clickable: false,
-            editable: true,
-            zIndex: 1
-        },
-        map: map
-    });
-
     var searchBox = $('#search'),
         submitButton = $('#submitButton'),
-        clearButton = $('#clearButton'),
         theList = $('#sidebar ul');
 
     var isEmpty = true,
         labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-        labelIndex = 0,
-        markerArray = [];
-
-
-    clearButton.click(function () {
-        markerArray.forEach(function (element) {
-            element.setMap(null);
-            labelIndex = 0;
-        });
-    });
+        labelIndex = 0;
 
     submitButton.click(function () {
         $.getJSON("http://api.geonames.org/wikipediaSearch?q=" + searchBox.val() + "&maxRows=20&username=shmuels&type=json&callback=?",
@@ -76,9 +47,8 @@ function initMap() {
                                 animation: google.maps.Animation.DROP,
                                 title: element.title,
                                 label: labels[labelIndex++ % labels.length],
+                                
                             });
-
-                            markerArray.push(marker);
 
                             var contentString =
                                 '<div style=float:left;><img src=' + element.thumbnailImg +
