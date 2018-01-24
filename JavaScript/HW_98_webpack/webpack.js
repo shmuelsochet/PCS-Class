040eca1e365f72ddd2bf429c93df679d33154b8c
ssochet <sysochet@gmail.com>
2018-01-23 19:26:54 -0500

    Use require jquery. Remove module design.

--- JavaScript/HW_98_webpack/webpack.js
+++ JavaScript/HW_98_webpack/webpack.js
@@ -1,14 +1,14 @@
-/* global $ */
-(function () {
-    "use strict";
 
-    var box = $('#box'),
-        colorPicker = $('#colorPicker');
+"use strict";
 
-    box.css('margin-bottom', '5px');
+const jquery = require('jquery');
 
-    colorPicker.change(function () {
-        box.css('background-color', colorPicker.val());
-    });
+var box = $('#box'),
+    colorPicker = $('#colorPicker');
 
-}());
+box.css('margin-bottom', '5px');
+
+colorPicker.change(function () {
+    box.css('background-color', colorPicker.val());
+});
+
