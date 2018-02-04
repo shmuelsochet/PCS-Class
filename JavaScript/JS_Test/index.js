a9337f6c7e6e431e422023c591c33fd312a00138
ssochet <sysochet@gmail.com>
2018-02-04 15:35:30 -0500

    Add Fetch API, along with error handling.

--- JavaScript/JS_Test/index.js
+++ JavaScript/JS_Test/index.js
@@ -35,127 +35,148 @@
 
     });
 
+    function networkError(response) {
+        throw Error(response);
+    }
+
+    function checkResponse(response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw Error(response.statusText);
+    }
+
     function displayBlog() {
 
-        $.get('https://jsonplaceholder.typicode.com/users', function (users) {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(checkResponse, networkError)
+            .then(function (users) {
 
-            users.forEach(function (user) {
+                users.forEach(function (user) {
 
-                usersArray.push(user);
+                    usersArray.push(user);
 
-                blogUl.append(
-                    '<li class=blogHover id=' + user.id + '>' + user.name + '</li>' +
-                    '<ul>' +
-                    '<li>' + user.website + '</li>' +
-                    '<li>' + user.company.name + '</li>' +
-                    '<ul>' +
-                    '<li>' + user.company.catchPhrase + '</li>' +
-                    '<li>' + user.company.bs + '</li>' +
-                    '</ul>' +
-                    '</ul>'
-                );
+                    blogUl.append(
+                        '<li class=blogHover id=' + user.id + '>' + user.name + '</li>' +
+                        '<ul>' +
+                        '<li>' + user.website + '</li>' +
+                        '<li>' + user.company.name + '</li>' +
+                        '<ul>' +
+                        '<li>' + user.company.catchPhrase + '</li>' +
+                        '<li>' + user.company.bs + '</li>' +
+                        '</ul>' +
+                        '</ul>'
+                    );
 
-                $('#' + user.id).click(function () {
-                    showPosts(user.id);
+                    $('#' + user.id).click(function () {
+                        showPosts(user.id);
+                    });
+
                 });
 
-            });
 
-        }).fail(function (jqXHR) {
-            console.log(jqXHR);
-        });
+            })
+            .catch(function (error) {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
 
     }
 
     function showPosts(userId) {
-        $.get('https://jsonplaceholder.typicode.com/posts?userId=' + userId, function (posts) {
 
-            blogUl.empty();
-            blogUl.css('listStyle', 'initial');
+        fetch('https://jsonplaceholder.typicode.com/posts?userId=' + userId)
+            .then(checkResponse, networkError)
+            .then(function (posts) {
 
-            postsLength = posts.length;
+                blogUl.empty();
+                blogUl.css('listStyle', 'initial');
 
-            posts.forEach(function (post, index) {
+                postsLength = posts.length;
 
-                postsArray.push(post);
+                posts.forEach(function (post, index) {
 
-                if (index > 2) {
+                    postsArray.push(post);
 
-                    if (index % 3 === 0) {
-                        nextBlock = index;
-                        cssClass = '"hide ' + nextBlock + '"';
-                    } else {
-                        cssClass = '"hide ' + nextBlock + '"';
+                    if (index > 2) {
+
+                        if (index % 3 === 0) {
+                            nextBlock = index;
+                            cssClass = '"hide ' + nextBlock + '"';
+                        } else {
+                            cssClass = '"hide ' + nextBlock + '"';
+                        }
+
                     }
 
-                }
+                    // Append posts along with comment button attach id to div to append comment
+                    // and attach class to button to attach click-handler below.
+                    blogUl.append(
+                        '<div id=' + post.id + ' class=' + cssClass + '>' +
+                        '<li><strong>Title:</strong> ' + post.title + '</li>' +
+                        '<ul>' +
+                        '<li>' + post.body + '</li>' +
+                        '</ul>' +
+                        '<button class=-' + post.id + '>Comments</button>' +
+                        '<br><label>Name:</label><input id=name-' + post.id + '>' +
+                        '<br><label>Body:</label><input id=body-' + post.id + '>' +
+                        '<br><label>Email:</label><input id=email-' + post.id + '>' +
+                        '<br><button class=addComment-' + post.id + '>Add Comment</button>' +
+                        '</div>'
 
-                // Append posts along with comment button attach id to div to append comment
-                // and attach class to button to attach click-handler below.
-                blogUl.append(
-                    '<div id=' + post.id + ' class=' + cssClass + '>' +
-                    '<li><strong>Title:</strong> ' + post.title + '</li>' +
-                    '<ul>' +
-                    '<li>' + post.body + '</li>' +
-                    '</ul>' +
-                    '<button class=-' + post.id + '>Comments</button>' +
-                    '<br><label>Name:</label><input id=name-' + post.id + '>' +
-                    '<br><label>Body:</label><input id=body-' + post.id + '>' +
-                    '<br><label>Email:</label><input id=email-' + post.id + '>' +
-                    '<br><button class=addComment-' + post.id + '>Add Comment</button>' +
-                    '</div>'
+                    );
 
-                );
+                    // Add comment click handler
+                    $('.addComment-' + post.id).click(function () {
+                        addComment(post.id);
+                    });
 
-                // Add comment click handler
-                $('.addComment-' + post.id).click(function () {
-                    addComment(post.id);
+                    // Comment button click handler
+                    $('.-' + post.id).click(function () {
+                        showComments(post.id);
+                    });
+
                 });
 
-                // Comment button click handler
-                $('.-' + post.id).click(function () {
-                    showComments(post.id);
-                });
+                previousButton.show().click(showPreviousThreePosts);
 
+                nextButton.show().click(showNextThreePosts);
+
+            })
+            .catch(function (error) {
+                console.error('There has been a problem with your fetch operation:', error);
             });
-
-            previousButton.show().click(showPreviousThreePosts);
-
-            nextButton.show().click(showNextThreePosts);
-
-        }).fail(function (jqXHR) {
-            console.log(jqXHR);
-        });
     }
 
     function showComments(postId) {
 
         if ($('.-' + postId).text() === 'Comments') {
 
-            $.get('https://jsonplaceholder.typicode.com/posts/' + postId + '/comments', function (comments) {
+            fetch('https://jsonplaceholder.typicode.com/posts/' + postId + '/comments')
+                .then(checkResponse, networkError)
+                .then(function (comments) {
 
-                comments.forEach(function (comment) {
+                    comments.forEach(function (comment) {
 
-                    commentsArray.push(comment);
+                        commentsArray.push(comment);
 
-                    // Append comments
-                    $('#' + comment.postId + '> ul').append(
-                        '<ul class=' + comment.postId + '>COMMENT ' + comment.id + ':' +
-                        '<li><strong>Name: </strong>' + comment.name + '</li>' +
-                        '<ul>' +
-                        '<li>' + comment.body + '</li>' +
-                        '<li><em>' + comment.email + '</em></li>' +
-                        '</ul>'
+                        // Append comments
+                        $('#' + comment.postId + '> ul').append(
+                            '<ul class=' + comment.postId + '>COMMENT ' + comment.id + ':' +
+                            '<li><strong>Name: </strong>' + comment.name + '</li>' +
+                            '<ul>' +
+                            '<li>' + comment.body + '</li>' +
+                            '<li><em>' + comment.email + '</em></li>' +
+                            '</ul>'
 
-                    );
+                        );
+                    });
+
+                    $('.-' + postId).text('Hide Comments');
+
+                })
+                .catch(function (error) {
+                    console.error('There has been a problem with your fetch operation:', error);
                 });
-
-                $('.-' + postId).text('Hide Comments');
-
-
-            }).fail(function (jqXHR) {
-                console.log(jqXHR);
-            });
         } else {
 
             currentPost = postId;
@@ -172,15 +193,26 @@
 
     function addComment(postId) {
 
-        $.post('https://jsonplaceholder.typicode.com/posts/' + postId + '/comments',
+        var nameInput = $('#name-' + postId),
+            bodyInput = $('#body-' + postId),
+            emailInput = $('#email-' + postId);
+
+        fetch('https://jsonplaceholder.typicode.com/posts/' + postId + '/comments',
             {
+                method: 'POST',
+                body: JSON.stringify({
+                    postId: postId,
+                    name: nameInput.val(),
+                    body: bodyInput.val(),
+                    email: emailInput.val()
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
 
-                postId: postId,
-                name: $('#name-' + postId).val(),
-                body: $('#body-' + postId).val(),
-                email: $('#email-' + postId).val()
-            }, function (comment) {
-
+            })
+            .then(checkResponse, networkError)
+            .then(function (comment) {
 
                 // Append comments
                 $('#' + comment.postId + '> ul').append(
@@ -193,13 +225,13 @@
 
                 );
 
-                $('#name-' + postId).val('');
-                $('#body-' + postId).val('');
-                $('#email-' + postId).val('');
+                nameInput.val('');
+                bodyInput.val('');
+                emailInput.val('');
 
-
-            }).fail(function (jqXHR) {
-                console.log(jqXHR);
+            })
+            .catch(function (error) {
+                console.error('There has been a problem with your fetch operation:', error);
             });
     }
 
