ad4952b2832314471b0b159b0f6d3c47a7c9005a
ssochet <sysochet@gmail.com>
2018-02-17 21:24:59 -0500

    Add HW_104 react students

---
+++ JavaScript/HW_104_react/my-app/src/Student.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class Student extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     // this.state = { name: 'bob' };
+    // }
+
+
+    printScores(scores) {
+        const jsxScores = scores.map(score =>
+            <p>{score}</p>
+        );
+
+        return jsxScores;
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>
+                    {this.props.name}
+                </h2>
+                <p>Scores: {this.printScores(this.props.scores)} </p>
+            </div>
+        );
+    }
+
+}
