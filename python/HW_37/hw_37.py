95f26f7478653c3dc1e33f877777d0aae1e17fc6
Shmuel <shmuel@Shmuels-MacBook-Pro.local>
2020-11-22 19:57:58 -0500

    Python HW 37

---
+++ python/HW_37/hw_37.py
@@ -0,0 +1,6 @@
+name = 'Shmuel Sochet'
+address = '209 Lake Drive Ter'
+children = ['Dovi', 'Aviva', 'Yehuda', 'Rachel']
+print(f"Name: {name}. Address: {address}. Children: {children}.")
+print(name[::3])
+print(children[-2][1:-1])
